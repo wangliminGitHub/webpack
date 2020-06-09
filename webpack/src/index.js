a06b2f8cc698091dde57aabf2496d0847d4daee0
wanglimin <17629094718@163.com>
2020-06-09 23:08:38 +0800

    验证提交代码自动格式化功能

--- webpack/src/index.js
+++ webpack/src/index.js
@@ -1,7 +1,7 @@
 /*
  * @Author: your name
  * @Date: 2020-03-10 20:25:54
- * @LastEditTime: 2020-06-09 21:00:44
+ * @LastEditTime: 2020-06-09 23:07:53
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: /myStudent/webpack/src/index.js
@@ -9,4 +9,4 @@
 import '@babel/polyfill'
 
 const fn = () => console.log(1111)
-fn()
+fn();
